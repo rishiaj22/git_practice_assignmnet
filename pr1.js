8b555d2e4d340dbc78449723452361cfad5f09b8
rishiaj22 <shrivastavarishiraj198@gmail.com>
2023-10-25 21:41:23 +0530

    Palindrome

---
+++ pr1.js
@@ -0,0 +1,15 @@
+function checkPalindrome(N, str){
+    let palindrome = false;
+    for(i=0;i<N;i++){
+        if(str[0]=srtr[N-1] && str[1]=str[4] && str[2]==str[3]){
+            palindrome = true;
+        }
+    }
+    if(palindrome){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
+}
+checkPalindrome(6,"nrupul");
